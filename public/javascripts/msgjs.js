800ae6603210bd6d3aa367ddfce9d3a2982e2f71
cong25825933 <492899414@qq.com>
2016-06-19 23:26:10 +0800

    delete code

--- public/javascripts/msgjs.js
+++ public/javascripts/msgjs.js
@@ -2,9 +2,9 @@
  * Created by Yc on 2016/6/17.
  */
 
+window.onerror = null;
 
-
-(function(doc,win){
+(function(doc,win,$){
     var input = doc.getElementById('msg');
     var show  = doc.getElementById('msg-show');
     input.placeholder = ['1. 支持 MarkDown 语法','2. 自动保存','3. 支持网络多张图片粘贴'].join('\r\n\r\n');
@@ -47,6 +47,13 @@
     }
     var renderer = new marked.Renderer();
     marked.setOptions({
+        gfm: true,
+        tables: true,
+        breaks: true, //回车换行
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false,
         highlight: function (code) {
             return hljs.highlightAuto(code).value;
         }
@@ -71,6 +78,9 @@
         if(typeof USERNAME!=='undefined')
             win.localStorage.setItem(tag+'-'+USERNAME,md);
         show.innerHTML = marked(md,{renderer:renderer});
+        if(input.clientHeight<input.scrollHeight){
+            input.style.height = input.scrollHeight+20+'px';
+        }
     }
     if(typeof USERNAME!=='undefined') {
         var prev = win.localStorage.getItem(tag+'-'+USERNAME);
@@ -82,6 +92,17 @@
 
     $('[data-md]').each(function () {
         var md = this.dataset.md;
+        //单个回车是换行
         this.innerHTML = marked(md,{renderer:renderer});
     })
-})(document,window);
+    $('[role=bigger]').click(function (e) {
+        $('.container').removeClass('container').addClass('container-fluid');
+    })
+    $('[role=link-msg]').click(function (e) {
+        var x = $(this.hash).next().children().removeClass('blink')
+        setTimeout(function () {
+            x.addClass('blink').children('textarea').focus();
+        },0)
+    })
+
+})(document,window,jQuery);
