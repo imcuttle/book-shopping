f061c3a89e8be96ee8b425f3e7d9b8244a7f2bc9
cong25825933 <492899414@qq.com>
2016-06-20 19:28:09 +0800

    big update

--- public/javascripts/msgAnimate.js
+++ public/javascripts/msgAnimate.js
@@ -4,5 +4,6 @@
 
 window.onload = function () {
     $('.messages')
-        .css({'transform':'translate(0,120px)',opacity:1})
+        // .css({'transform':'translate(0,120px)',opacity:1})
+        .css({'margin-top':'140px',opacity:1})
 }
